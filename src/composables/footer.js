d10fb24d5477589d80bcde666ded9003f00f75d3
kimub <kimub1204@gmail.com>
2023-09-15 08:07:38 +0900

    feat: qr 리더 기능 구현

---
+++ src/composables/footer.js
@@ -0,0 +1,37 @@
+import homeImg from '@/assets/images/home.svg';
+import noticeImg from '@/assets/images/notice.svg';
+import qrImg from '@/assets/images/qr.svg';
+import curriculumImg from '@/assets/images/curriculum.svg';
+import mypageImg from '@/assets/images/mypage.svg';
+
+export function useFooter() {
+  const info = [
+    {
+      img: homeImg,
+      title: '홈',
+      route: 'HomeView',
+    },
+    {
+      img: noticeImg,
+      title: '공지사항',
+      route: 'NoticeView',
+    },
+    {
+      img: qrImg,
+      title: 'QR',
+      route: 'QrView',
+    },
+    {
+      img: curriculumImg,
+      title: '커리큘럼',
+      route: 'CurriculumView',
+    },
+    {
+      img: mypageImg,
+      title: '마이페이지',
+      route: 'MyPageView',
+    },
+  ];
+
+  return { info };
+}
--- src/layout/Header/Header.vue
+++ src/layout/Header/Header.vue
@@ -1,7 +1,34 @@
 <template>
-  <div>Header</div>
+  <header :class="classes.header">
+    <img :class="classes.logo" :src="logoImg" alt="로고" />
+    <h1 :class="classes.title">{{ getPageTitle() }}</h1>
+  </header>
 </template>
 
-<script setup></script>
+<script setup>
+import logoImg from '@/assets/images/logo.svg';
+import { useRoute } from 'vue-router';
 
-<style module="classes" scoped></style>
+const route = useRoute();
+
+const getPageTitle = () => {
+  switch (route.name) {
+    case 'HomeView':
+      return '홈';
+    case 'NoticeView':
+      return '공지사항';
+    case 'QrView':
+      return 'QR';
+    case 'CurriculumView':
+      return '커리큘럼';
+    case 'MyPageView':
+      return '마이페이지';
+    default:
+      return 'NotFound';
+  }
+};
+</script>
+
+<style module="classes" scoped>
+@import './Header.css';
+</style>
