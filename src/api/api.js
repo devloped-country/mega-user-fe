8cecc651b0f5d2b0ce7846ea0965821554ee927a
ubeom kim <kimub1204@gmail.com>
2023-09-24 17:43:35 +0900

    feat: 웹뷰에서 로그인 안되는 에러 보이는 기능 추가

--- src/api/api.js
+++ src/api/api.js
@@ -8,6 +8,66 @@ export const api = axios.create({
   },
 });
 
+export const apiClient = axios.create({
+  baseURL: 'https://api.megamega-app.com',
+});
+
+apiClient.interceptors.request.use(
+  async function (config) {
+    const loginUrl = 'https://mega-user.vercel.app/';
+    const joinUrl = 'https://mega-user.vercel.app/join';
+
+    if (location.href === loginUrl || location.href === joinUrl) {
+      return config;
+    }
+
+    if (!localStorage.getItem('access') || !localStorage.getItem('refresh')) {
+      localStorage.clear();
+      location.href = loginUrl;
+      return;
+    }
+
+    try {
+      const res = await fetch('/auth', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('access')}`,
+          'Token-Kind': 'access',
+        },
+      });
+
+      if (res.status === 401) {
+        throw new Error();
+      }
+
+      return config;
+    } catch (err) {
+      try {
+        const res = await fetch('/auth', {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('refresh')}`,
+            'Token-Kind': 'refresh',
+          },
+        }).then((res) => res.json());
+
+        localStorage.setItem('access', res.access);
+        if (res.refresh) {
+          localStorage.setItem('refresh', res.refresh);
+        }
+
+        return config;
+      } catch (error) {
+        localStorage.clear();
+        location.href = loginUrl;
+      }
+    }
+
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
 api.interceptors.request.use(
   async function (config) {
     const loginUrl = 'https://mega-user.vercel.app/';
